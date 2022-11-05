583517dd562b3da04401a3cf75faf40c3a95af24
Alexander Megadrive <alexmihailov91@gmail.com>
2022-11-05 12:35:40 +0300

    feat: finish task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
+const fs = require('fs')
+const fsPromises = require("fs").promises;
+const path = require('path');
+const {
+    stdin,
+    stdout
+} = process;
+const {
+    copyFile,
+    readdir,
+    mkdir
+} = require('node:fs/promises')
+
+let pathName = path.join(__dirname, 'files')
+let pathNew = path.join(__dirname, 'files-copy')
+
+copyFolder()
+async function copyFolder() {
+    fs.mkdir(pathNew, { recursive: true }, err => {
+        if(err) throw err; // не удалось создать папку
+        console.log('папка создана');
+     });
+
+     try {
+        const files = await readdir(pathName, {
+        });
+        for (const file of files) {
+        try {
+            await copyFile(`${pathName}\\${file}`, `${pathNew}\\${file}`);
+            console.log(`файл ${file} скопирован`);
+          } catch(error) {
+            console.log(error);
+          }
+       } 
+    
+    } catch (err) {
+        console.error(err);
+    }
+
+}
+
 
